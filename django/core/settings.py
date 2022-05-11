aee539cc4d46fe3b75699a7b628238266e2ae06c
govind-raj99 <rajvind63@gmail.com>
2022-05-11 14:55:40 +0800

    Update settings.py

--- django/core/settings.py
+++ django/core/settings.py
@@ -8,7 +8,7 @@ from decouple import config
 from unipath import Path
 import dj_database_url
 
-#comment
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).parent
 CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
